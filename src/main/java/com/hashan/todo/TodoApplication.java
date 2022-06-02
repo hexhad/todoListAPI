3acf8cda7b6f9619ff6a8124bf51a7ec2f0ae81e
hexhad <hexhad@gmail.com>
2022-06-02 15:41:23 -0700

    swagger integration

--- src/main/java/com/hashan/todo/TodoApplication.java
+++ src/main/java/com/hashan/todo/TodoApplication.java
@@ -2,11 +2,14 @@ package com.hashan.todo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
+@EnableSwagger2
 public class TodoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
-		System.out.println("\nPATH -> localhost:8081/api\n");
+		System.out.println("\nPATH -> http://localhost:8081/api");
+		System.out.println("\nSWAGGER -> http://localhost:8080/swagger-ui.html\n");
 	}
 }
