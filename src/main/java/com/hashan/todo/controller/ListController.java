74d3a6f563a81c7834da44014f543c735142b508
hexhad <hexhad@gmail.com>
2022-06-03 19:13:09 -0700

    csrf issue fixed

--- src/main/java/com/hashan/todo/controller/ListController.java
+++ src/main/java/com/hashan/todo/controller/ListController.java
@@ -16,24 +16,28 @@ public class ListController {
     TaskService taskService;
 
     //CREATE task
+    @CrossOrigin
     @PostMapping("/create")
     public TaskList createTask(@RequestBody @Validated TaskList taskList){
         return taskService.createTask(taskList);
     }
 
     //READ tasks
+    @CrossOrigin
     @GetMapping("/read-all")
     public List<TaskList> readAllTasks(){
         return taskService.readAllTasks();
     }
 
     //READ task
+    @CrossOrigin
     @GetMapping("/read-one/{task}")
     public TaskList readTask(@PathVariable (value = "task", required = true) Integer task){
         return taskService.readTask(task);
     }
 
     //UPDATE task
+    @CrossOrigin
     @PutMapping("/update")
     public TaskList updateTask(@RequestBody @Validated TaskList taskList){
         return taskService.updateTask(taskList);
@@ -41,8 +45,10 @@ public class ListController {
 
 
     //DELETE task
+    @CrossOrigin
     @DeleteMapping("/delete/{task}")
     public void deleteTask(@PathVariable (value = "task", required = true) Integer task){
         taskService.deleteTask(task);
     }
+
 }
